766a355de6d64133c2df8a42694d317072a161a8
hpacatang <hadrielpacatang@gmail.com>
2025-04-14 14:26:18 +0800

    modified Property controller, model, api and table

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,26 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\PropertyController;
 
+//these routes are for the properties
+Route::get('/properties', [PropertyController::class, 'index']);
+Route::get('/properties/{id}', [PropertyController::class, 'show']);
+Route::post('/properties', [PropertyController::class, 'store']);
+Route::put('/properties/{id}', [PropertyController::class, 'update']);
+Route::delete('/properties/{id}', [PropertyController::class, 'destroy']);
+Route::get('/properties/area/{area}', [PropertyController::class, 'filterByArea']);
 
+//these routes are for the events
+Route::post('/events', [EventController::class, 'store']);
+Route::get('/events', [EventController::class, 'index']);
+Route::get('/events/{id}', [EventController::class, 'show']);
+Route::post('/events', [EventController::class, 'store']);
+Route::put('/events/{id}', [EventController::class, 'update']);
+Route::delete('/events/{id}', [EventController::class, 'destroy']);
+
+//these are for handling user authentication
 Route::post('/register', [UserController::class, 'register']);
 Route::post('/login', [UserController::class,'login']);
 Route::post('/login/verify', [UserController::class,'verify']);
--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TemplateController;
+use App\Http\Controllers\EventController;
+
 
 Route::get('/', [TemplateController::class,'index']);
