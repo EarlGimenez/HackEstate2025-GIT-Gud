489dd75dea0fdc7f93fb5225a92c526d6978e479
EarlGimenez <earl.reynan123@gmail.com>
2025-04-14 13:48:29 +0800

    Project Update: Seperated API and Web Calls

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\TemplateController;
 
 Route::get('/', [TemplateController::class,'index']);
+
+Route::post('/login', [UserController::class,'login']);
+Route::post('/logout', [UserController::class,'logout']);
