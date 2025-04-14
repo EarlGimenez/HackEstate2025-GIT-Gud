486c3b63cc655af95765790ab63e0402d92b2289
EarlGimenez <earl.reynan123@gmail.com>
2025-04-14 11:18:09 +0800

    Project Update: First View Landing Page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TemplateController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [TemplateController::class,'index']);
